92285986ed213465beee8d7fc609a528562a20fb
Mustafa Uçar <mucarr419@gmail.com>
2023-02-12 19:41:14 +0300

    Project Created

---
+++ lib/Screens/HomePage.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Flutter",
+                style: TextStyle(
+                  fontSize: 26,
+                ),
+
+            ),
+            Text("Blog",style: TextStyle(fontSize: 26,color: Colors.red),),
+
+          ],
+
+        ),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: (){
+              // Tıklayınca gönderiyi firebase ekle
+          },
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.file_upload))),
+        ],
+      ),
+    );
+  }
+}
